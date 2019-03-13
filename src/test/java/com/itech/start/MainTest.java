89b4c80ba9072d50f90f27b26578e9ec0bf64a08
Pavel.Khatamtsou <https://bitbucket.org/GraF_PaVeL/>
2019-03-13 13:14:48 +0300

    test temp class

---
+++ src/test/java/com/itech/start/MainTest.java
@@ -0,0 +1,19 @@
+package com.itech.start;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void sum1() {
+        Assert.assertEquals(5,Main.sum(4,1));
+    }
+
+    @Test
+    public void sum2() {
+        Assert.assertEquals(3,Main.sum(2,1));
+    }
+}
