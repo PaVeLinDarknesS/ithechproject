28011ab223c57355508c2d837bff72ac03f829ea
Pavel.Khatamtsou <https://bitbucket.org/GraF_PaVeL/>
2019-03-25 14:22:03 +0300

    Fix version plugin

--- src/main/java/com/itech/library/contoller/HelloController.java
+++ src/main/java/com/itech/library/contoller/HelloController.java
@@ -21,24 +21,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String getHelloPage() {
-        userRepository.getUserById(1);
-        userRepository.getUserByLogin("Admin");
-        userRepository.addUser(new User("test", "test"));
-        userRepository.findOne(new User("test", "test"));
-        return "hello";
-    }
-
-    @RequestMapping("/testBook")
-    public String testBook() {
-        Optional<Author> authorId1 = authorRepository.getAuthorById(1);
-        Optional<Author> authorId0 = authorRepository.getAuthorById(0);
-
-        Author addAut = new Author("Test", "Test");
-        Author authorAdd1 = authorRepository.addAuthor(addAut);
-        Author authorAdd2 = authorRepository.addAuthor(addAut);
-        authorAdd1.setFirstName("Temp");
-        Author updateAuthor = authorRepository.updateAuthor(authorAdd1);
-//        Author deleteAuthor = authorRepository.deleteAuthor(updateAuthor);
         return "hello";
     }
 
