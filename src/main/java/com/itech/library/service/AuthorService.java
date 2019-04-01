80d62f821a3769ca5b85f6fcc7a844bb207c5348
Pavel.Khatamtsou <https://bitbucket.org/GraF_PaVeL/>
2019-04-01 11:48:39 +0300

    - Change method in AuthorRepository form "findOne" to "getAuthorByFio" - Refactor AuthorService

--- src/main/java/com/itech/library/service/AuthorService.java
+++ src/main/java/com/itech/library/service/AuthorService.java
@@ -2,15 +2,17 @@ package com.itech.library.service;
 
 public interface AuthorService {
 
-//TODO Write
-    /*
-    Optional<Author> getAuthorById(int id);
-    Author addAuthor(Author author);
-    Author updateAuthor(Author author);
-    Author deleteAuthor(Author author);
-    List<Author> getAllAuthors();
-    Optional<Author> findOne(Author author);
-    void addBookInAuthor(Book book, Author author);
-    void removeBookInAuthor(Book book, Author author);
-    */
+    //Optional<AuthorPojo> getAuthorByFio(String firstName, String lastName);
+
+    //List<AuthorPojo> getAllAuthors();
+
+    //AuthorPojo addAuthor(AuthorPojo author);
+
+    //AuthorPojo updateAuthor(AuthorPojo author);
+
+    //AuthorPojo deleteAuthor(AuthorPojo author);
+
+    //boolean addBookInAuthor(BookPojo book, AuthorPojo author);
+
+    //boolean removeBookInAuthor(BookPojo book, AuthorPojo author);
 }
---
+++ src/test/java/com/itech/library/service/impl/AuthorServiceImplTest.java
@@ -0,0 +1,8 @@
+package com.itech.library.service.impl;
+
+import static org.junit.Assert.*;
+
+public class AuthorServiceImplTest {
+
+
+}
